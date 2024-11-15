ccab18fc3708aed27e99d8ab2d65c2c0f32407c8
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-15 16:54:47 +0700

    question 4

--- ArrayValue11.java
+++ ArrayValue11.java
@@ -3,12 +3,16 @@ public class ArrayValue11 {
     public static void main(String[] args) {
         int[] finalScore = new int[10];
         Scanner input11 = new Scanner(System.in);
-        for (int i = 0; i < finalScore.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print("Enter the final score " + i + ": ");
             finalScore[i] = input11.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Final score " + i + " is " + finalScore[i]);
+        for (int i = 0; i < finalScore.length; i++) {
+            if (finalScore[i] > 70 ) {
+            System.out.println("Student " + i + " Passed!"); 
+            } else {
+                System.out.println("Student " + i + " Failed!");
+            }
         }
     }
 }
