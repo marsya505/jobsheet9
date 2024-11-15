d54c256d84def296f476b868940a8de50de8ddd8
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-15 11:20:09 +0700

    question 1

--- ArrayValue11.java
+++ ArrayValue11.java
@@ -3,7 +3,7 @@ public class ArrayValue11 {
     public static void main(String[] args) {
         int[] finalScore = new int[10];
         Scanner input11 = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter the final score " + i + ": ");
             finalScore[i] = input11.nextInt();
         }
