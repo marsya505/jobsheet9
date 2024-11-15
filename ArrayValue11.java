5eb4be04ddd8e4e2c14ef3ad79a35785af98ae5c
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-15 09:53:05 +0700

    Experiment 2

---
+++ ArrayValue11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ArrayValue11 {
+    public static void main(String[] args) {
+        int[] finalScore = new int[10];
+        Scanner input11 = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = input11.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score " + i + " is " + finalScore[i]);
+        }
+    }
+}
