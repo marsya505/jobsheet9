307f5dbef4591057a3be9f1ec4b2e544076bcb3f
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-15 10:26:06 +0700

    question 1

--- ArrayNumbers11.java
+++ ArrayNumbers11.java
@@ -1,10 +1,10 @@
 public class ArrayNumbers11 {
     public static void main(String[] args) {
         int[] num = new int[4];
-        num[0] = 5;
-        num[1] = 12;
-        num[2] = 7;
-        num[3] = 20;
+        num[0] = 5.0;
+        num[1] = 12867;
+        num[2] = 7.5;
+        num[3] = 2000000;
         System.out.println(num[0]);
         System.out.println(num[1]);
         System.out.println(num[2]);
